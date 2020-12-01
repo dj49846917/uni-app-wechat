dcfc9b503eeafdf6ea84b82abf06b1044d33b35d
dj49846917 <821084785@qq.com>
2020-12-01 18:02:38 +0800

    '首页开发'

---
+++ static/data/home.js
@@ -0,0 +1,54 @@
+const listData = [
+	{
+		id: Math.random(),
+		avatar: "/static/images/userpic.jpg",
+		noreadnum: 2,
+		name: "张三",
+		update_time: 1606792814,
+		data: "你在吗2323232你在吗2323232你在吗2323232你在吗2323232"
+	},
+	{
+		id: Math.random(),
+		avatar: "/static/images/userpic.jpg",
+		noreadnum: 66,
+		name: "张四",
+		update_time: 1604200800,
+		data: "你在吗"
+	},
+	{
+		id: Math.random(),
+		avatar: "/static/images/userpic.jpg",
+		noreadnum: 25,
+		name: "张五",
+		update_time: 1600399200,
+		data: "而认为吴王夫差拖后腿"
+	},
+	{
+		id: Math.random(),
+		avatar: "/static/images/userpic.jpg",
+		noreadnum: 2,
+		name: "张六",
+		update_time: 1600312800,
+		data: "局研讨会让我问问"
+	},
+	{
+		id: Math.random(),
+		avatar: "/static/images/userpic.jpg",
+		noreadnum: 12,
+		name: "张七",
+		update_time: 1600053600,
+		data: "热热热提"
+	},
+	{
+		id: Math.random(),
+		avatar: "/static/images/userpic.jpg",
+		noreadnum: 55,
+		name: "张八",
+		update_time: 1594696800,
+		data: "驱蚊器翁群翁无群"
+	},
+]
+
+export {
+	listData
+}
