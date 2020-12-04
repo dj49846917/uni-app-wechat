4e95e23a83822f0e4096bcf5267053883de35972
dj49846917 <821084785@qq.com>
2020-12-04 17:27:42 +0800

    '页面'

---
+++ static/data/look.js
@@ -0,0 +1,75 @@
+const lookMenuData = [
+	{
+		id: Math.random(),
+		avatar: "",
+		val: "朋友圈",
+		icon: "\ue60d",
+		noReadInfo: {
+			text: "",
+			imgUrl: "/static/images/userpic.png"
+		}
+	},
+	{
+		id: Math.random(),
+		avatar: "",
+		val: "视频号",
+		icon: "\ue66f",
+		noReadInfo: {
+			text: "朋友在看直播朋友在看直播朋友在看直播朋友在看直播朋友在看直播朋友在看直播",
+			imgUrl: "/static/images/userpic.png"
+		}
+	},
+	{
+		id: Math.random(),
+		avatar: "",
+		val: "扫一扫",
+		icon: "\ue624",
+	},
+	{
+		id: Math.random(),
+		avatar: "",
+		val: "摇一摇",
+		icon: "\ue623"
+	},
+	{
+		id: Math.random(),
+		avatar: "",
+		val: "看一看",
+		icon: "\ue60c"
+	},
+	{
+		id: Math.random(),
+		avatar: "",
+		val: "搜一搜",
+		icon: "\ue611"
+	},
+	{
+		id: Math.random(),
+		avatar: "",
+		val: "附近的人",
+		icon: "\ue646"
+	},
+	{
+		id: Math.random(),
+		avatar: "",
+		val: "购物",
+		icon: "\ue67e"
+	},
+	{
+		id: Math.random(),
+		avatar: "",
+		val: "游戏",
+		icon: "\ue60b"
+	},
+	{
+		id: Math.random(),
+		avatar: "",
+		val: "小程序",
+		icon: "\ue81e"
+	}
+]
+
+
+export {
+	lookMenuData
+}
